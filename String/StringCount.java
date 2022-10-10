27858bf38819e8325e7ba395ff82c78906e0ca20
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-10-10 11:41:59 +0530

    string

---
+++ String/StringCount.java
@@ -0,0 +1,22 @@
+package String;
+
+public class StringCount {
+    public static void main(String[] args) {
+        String name="vivek Gupta";
+        int count=1;
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+            
+
+
+        }
+        System.out.println(count);
+        
+        System.out.println("Substring:"+name.substring(0, 6));
+
+        // String are immutable
+        name.concat("newVivek");
+        System.out.println("updated:"+name);
+    }
+    
+} 
